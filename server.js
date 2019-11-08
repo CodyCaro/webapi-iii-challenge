084583926820c9129b0c3acba8b16939c36711c9
Cody Caro <codycaro@Codys-MacBook-Pro.local>
2019-11-07 16:45:03 -0800

    finished the logger middleware method

--- server.js
+++ server.js
@@ -1,15 +1,26 @@
-const express = 'express';
+const express = require("express");
+const helmet = require("helmet");
+const morgan = require("morgan");
 
 const server = express();
 
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`)
+server.use(express.json());
+server.use(helmet());
+server.use(morgan("dev"));
+server.use(logger);
+
+server.get("/", (req, res) => {
+  res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
 function logger(req, res, next) {
-
-};
+  console.log(`${req.method}  Request`);
+  console.log(`${req.url}  Request URL`);
+  let timeStamp = new Date();
+  console.log(`${timeStamp}  Timestamp`);
+  next();
+}
 
 module.exports = server;
